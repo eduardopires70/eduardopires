0a87fb96e1bfb2e92ea70009f7868d92385d2304
eduardopires70 <eduardo.pires@outlook.com>
2025-03-18 19:11:49 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+    
+    if (nome === "") {
+        alert("Favor digitar um nome válido.");
+        return;
+    }
+    
+    if (amigos.includes(nome)) {
+        alert("Este nome já foi incluído. Escolha outro.");
+        return;
+    }
+    
+    amigos.push(nome);
+    atualizarLista();
+    input.value = "";
+}
+
+function atualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    amigos.forEach(nome => {
+        const li = document.createElement("li");
+        li.textContent = nome;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("É necessário que a lista possua ao menos um nome.");
+        return;
+    }
+    
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+    
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    
+    const li = document.createElement("li");
+    li.textContent = `E o amigo sorteado é: ${amigoSorteado}`;
+    resultado.appendChild(li);
+}
